4535e991cc6f5864c2a195d1e99b622cb8d5ef0b
he <bgyhenrietta@yahoo.com>
2021-11-05 20:14:16 +0100

    week04 day03 stock

---
+++ week04-labs-tasks/src/main/java/day03/Stock.java
@@ -0,0 +1,29 @@
+package day03;
+
+import java.util.List;
+
+public class Stock {
+
+    private List<Double> values;
+
+    public Stock(List<Double> values) {
+        this.values = values;
+    }
+
+    public double maxProfit() {
+        double max = 0;
+        for (int i = 0; i < values.size(); i++) {
+            max = checkDailyDifferences(max, i);
+        }
+        return max;
+    }
+
+    public double checkDailyDifferences(double max, int i) {
+        for ( int j = i + 1; j < values.size(); j++) {
+            if (max < values.get(j) - values.get(i)) {
+                max = values.get(j) - values.get(i);
+            }
+        }
+        return max;
+    }
+}
